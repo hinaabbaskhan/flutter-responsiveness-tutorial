0ea65d6f9a16ccd9103c3a968050e085c989eb6b
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-02-24 15:17:59 +0500

    setup device preview

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,12 @@ import 'package:flutter/services.dart';
 import 'package:flutter_responsiveness_tutorial/check_email.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+// void main() => runApp(
+//       DevicePreview(
+//         enabled: !kReleaseMode,
+//         builder: (context) => MyApp(), // Wrap your app
+//       ),
+//     );
 void main() {
   runApp(const MyApp());
 }
@@ -17,16 +23,19 @@ class MyApp extends StatelessWidget {
       DeviceOrientation.portraitDown,
     ]);
     return ScreenUtilInit(
-        designSize: const Size(393, 852),
-        minTextAdapt: true,
-        splitScreenMode: true,
-        builder: (_, child) {
-          return MaterialApp(
-              theme:
-                  ThemeData(appBarTheme: AppBarTheme(color: Colors.deepPurple)),
-              home: CheckEmail()
-              // HomeScreen(),
-              );
-        });
+      designSize: const Size(393, 852),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return MaterialApp(
+            // locale: DevicePreview.locale(context),
+            // builder: DevicePreview.appBuilder,
+            theme:
+                ThemeData(appBarTheme: AppBarTheme(color: Colors.deepPurple)),
+            home: CheckEmail()
+            // HomeScreen(),
+            );
+      },
+    );
   }
 }
